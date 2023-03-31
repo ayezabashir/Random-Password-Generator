01a4779c729b3be96073407a2290159862a80277
Ayeza <ayezabashir442@gmail.com>
2023-04-01 00:23:21 +0500

    generateRandomPassword

--- js/main.js
+++ js/main.js
@@ -18,3 +18,36 @@ const getRandom = (dataSet) => {
     return dataSet[Math.floor(Math.random(dataSet) * dataSet.length)];
 }
 
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    }
+    else {
+        return str;
+    }
+}
+
+const generateRandomPass = (password = "") => {
+    if (upperInput.checked) {
+        password += getRandom(upperSet);
+    }
+    if (lowerInput.checked) {
+        password += getRandom(lowerSet);
+    }
+    if (numberInput.checked) {
+        password += getRandom(numSet);
+    }
+    if (symbolInput.checked) {
+        password += getRandom(symbolSet);
+    }
+    if (password.length < totalChar.value) {
+        return generateRandomPass(password);
+    }
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+generateRandomPass();
+
+document.getElementById("btn").addEventListener("click", function () {
+    generateRandomPass();
+})
